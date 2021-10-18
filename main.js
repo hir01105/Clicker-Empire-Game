7cf7fba7dce62c0491ee7176b8d4b13163f6b694
hir01105 <engineer.hiro1105@gmail.com>
2021-10-19 03:00:34 +0900

    Added buttons' functions and logics

--- main.js
+++ main.js
@@ -28,28 +28,47 @@ class Item{
         
     }
 
+    getProfitDescription(){
+        let profitStr = "";
+        if (this.type == "ability"){
+            profitStr = "¥" + this.profit + " /click";
+        }else if(this.type == "investment"){
+            profitStr = "¥" + this.profit + "% /sec";
+        }else{
+            profitStr = "¥" + this.profit + " /sec";
+        }
+        return profitStr;
+    }
+
     updateEtfPrice(){
         this.price = this.price * 1.1;
         return this.price;
     }
 
     updateEtfProfitPerSec(){
-        this.profitPerSec = this.totalValue * this.profit  
+        this.profitPerSec = Math.floor(this.totalValue * this.profit);  
     }
 
     updateTotalValue(num){
         this.totalValue += this.price * num;
     }
     
-    buyItem(num){
+    increaseItem(num){
+        let isAbility = false;
         this.number += num;
+        if(isFinite(this.max)){
+            this.max -= num;
+        }
         this.updateTotalValue(num);
         if (this.type == "investment"){
             this.updateEtfProfitPerSec();
             this.updateEtfPrice();
-        } else if(this.type == "real estate"){
+        }else if(this.type == "real estate"){
             this.profitPerSec = this.profit * this.number;
+        }else{
+            isAbility = true;
         }
+        return isAbility;
     }
 }
 
@@ -71,6 +90,21 @@ class UserAccount{
             arg += this.items[i].profitPerSec;
         }
         this.profitPerSec = arg;
+    }
+
+    clickBurger(){
+        this.money += this.oneClick;
+        this.burger += 1;
+    }
+
+    buyItem(index, number){
+        let targetItem = this.items[index];
+        const abilityCheck = targetItem.increaseItem(number);
+        if(abilityCheck){
+            this.oneClick += targetItem.profit;
+        }
+        this.money -= targetItem.price * number;
+        this.updateProfitPerSec();
     }
 }
 
@@ -94,14 +128,7 @@ function createItemContainer(itemArray){
     container.id = "selectItem";
     for(let i=0; i < itemArray.length;i++){
         let currItem = itemArray[i];
-        let profitStr = "";
-        if (currItem.type == "ability"){
-            profitStr = "¥" + currItem.profit + " /click";
-        }else if(currItem.type == "investment"){
-            profitStr = "¥" + currItem.profit + "% /sec";
-        }else{
-            profitStr = "¥" + currItem.profit + " /sec";
-        }
+        let profitStr = currItem.getProfitDescription();
 
         htmlString += 
         `
@@ -136,23 +163,27 @@ function createInfoCon(UserAccount){
     `;
 
     let element = UserAccount.name;
+    let eleClassName = "userName";
     for(let i=0;i < 4;i++){
         console.log(infoCon.innerHTML)
         switch(i){
             case 1:
                 element = UserAccount.age + " years old";
+                eleClassName = "userAge";
                 break;
             case 2:
                 element = UserAccount.day + " days";
+                eleClassName = "userDays";
                 break;
             case 3:
                 element = "¥" + UserAccount.money;
+                eleClassName = "userMoney";
         }
         htmlString += 
         `
         <div class="col-12 col-sm-6 p-1">
-            <div class="bg-pink text-center text-white p-2">
-                <h6>${element}</h6>
+            <div class="bg-pink text-center text-white p-2 userInfo">
+                <h6 class=${eleClassName} id=${eleClassName}>${element}</h6>
             </div>
         </div>
         `;
@@ -163,7 +194,6 @@ function createInfoCon(UserAccount){
     </div>
     `;
     infoCon.innerHTML = htmlString;
-    console.log(infoCon);
     return infoCon;
 }
 
@@ -180,6 +210,83 @@ function createResetAndSave(){
     </div>
     `;
     return container;
+}
+
+function createSubPage(UserAccount, value){
+    let itemList = document.getElementById("selectItem");
+    displayNone(itemList);
+    let subPage = document.createElement("div");
+    let intValue = parseInt(value);
+    let targetItem = UserAccount.items[intValue];
+    let maxPurchaseStr = targetItem.max;
+    if(maxPurchaseStr == "Infinity"){
+        maxPurchaseStr = "∞";
+    }
+    let htmlString = 
+    `
+    <div class="bg-pink p-2 m-sm-1 m-xs-2 text-white">
+        <div class="d-flex justify-content-between align-items-center">
+            <div>
+                <h4>${targetItem.name}</h4>
+                <p>Max purchases: ${maxPurchaseStr}</p>
+                <p>Price: ¥${targetItem.price}</p>
+                <p>Get ${targetItem.getProfitDescription()}</p>
+            </div>
+            <div>
+                <img src=${targetItem.imageUrl} class="middleImg-fit">
+            </div>
+        </div>
+        <p>How many would you like to buy?</p>
+        <input placeholder="0" type="number" class="col-12 form-control quantity" min="1">
+        <p class="text-right total-view"></p>
+        <div class="d-flex justify-content-between pb-3">
+            <button class="btn-blue col-5 back-btn">Go Back</button>
+            <button class="btn-pink col-5 purchase-btn">Purchase</button>
+        </div>
+    </div>
+    `;
+
+    subPage.innerHTML = htmlString;
+    let inputForm = subPage.querySelectorAll(".quantity")[0];
+    inputForm.addEventListener("change", (ele) => {
+        let totalView = subPage.querySelectorAll(".total-view")[0];
+        let total = ele.target.value * targetItem.price;
+        totalView.innerHTML = "total: ¥" + total;
+    })
+
+    let backBtn = subPage.querySelectorAll(".back-btn")[0];
+    backBtn.addEventListener("click", function(){
+        createMainPage(UserAccount);
+    })
+
+    let purchaseBtn = subPage.querySelectorAll(".purchase-btn")[0];
+    purchaseBtn.addEventListener("click", function(){
+        
+        if(inputForm.value == "" || parseInt(inputForm.value) < 0){
+            createMainPage(UserAccount);
+        }else if(UserAccount.money < (parseInt(inputForm.value) * targetItem.price)){
+            alert("You don't have enough money.");
+            createMainPage(UserAccount);
+        }else if(targetItem.max < parseInt(inputForm.value)){
+            alert("You cannot buy anymore.")
+            createMainPage(UserAccount);
+        }else{ 
+            UserAccount.buyItem(intValue, parseInt(inputForm.value));
+            createMainPage(UserAccount);
+        }
+    })
+
+    document.querySelectorAll(".item-box")[0].append(subPage);
+}
+
+function updateUserMoney(UserAccount){
+    let userMoney = document.getElementById("userMoney");
+    userMoney.innerHTML = "¥" + UserAccount.money;
+}
+
+function updateBurger(UserAccount){
+    let userBurger = document.getElementById("userBurger");
+    userBurger.innerHTML = UserAccount.burger + " Burgers";
 }
 
 function initializeUserAccount(){
@@ -205,13 +312,14 @@ function createMainPage(UserAccount){
     leftSide.innerHTML =
     `
     <div class="bg-pink pt-1 text-white text-center">
-        <h6>${UserAccount.burger} Burgers</h6>
-        <p>one click ¥${UserAccount.oneClick}</p>
+        <h6 id="userBurger">${UserAccount.burger} Burgers</h6>
+        <p id="userOneClick">one click ¥${UserAccount.oneClick}</p>
     </div>
     <div class="p-2 pt-sm-5 d-flex justify-content-center">
-        <img src="https://cdn-icons.flaticon.com/png/512/1811/premium/1811974.png?token=exp=1634361657~hmac=702ef0ac8c4f857ea597b2919beddb68" class="img-fit hover">
+        <img src="https://cdn-icons.flaticon.com/png/512/1811/premium/1811974.png?token=exp=1634361657~hmac=702ef0ac8c4f857ea597b2919beddb68" class="img-fit hover burger">
     </div>
     `;
+    
     mainTab.append(leftSide);
 
     let rightside = document.createElement("div");
@@ -219,13 +327,29 @@ function createMainPage(UserAccount){
     rightside.append(createInfoCon(UserAccount));
     
     let whiteBoxContainer = document.createElement("div");
-    whiteBoxContainer.classList.add("white-box", "mt-2", "p-1", "overflow-auto", "flowHeight");
+    whiteBoxContainer.classList.add("item-box","white-box", "mt-2", "p-1", "overflow-auto", "flowHeight");
     whiteBoxContainer.append(createItemContainer(UserAccount.items));
     rightside.append(whiteBoxContainer);
     rightside.append(createResetAndSave());
 
     mainTab.append(rightside);
     container.append(mainTab);
+
+    //set button functions
+    let burgerImage = leftSide.querySelectorAll(".burger")[0];
+    burgerImage.addEventListener("click", function(){
+        UserAccount.clickBurger();
+        updateUserMoney(UserAccount);
+        updateBurger(UserAccount);
+    })
+
+    let itemBoxes = whiteBoxContainer.querySelectorAll(".item");
+    for(let i=0;i < itemBoxes.length;i++){
+        let currItem = itemBoxes[i];
+        currItem.addEventListener("click", function() {
+            createSubPage(UserAccount, currItem.getAttribute("data-index"));
+        })
+    }
 
     config.mainPage.append(container);
     //return container
